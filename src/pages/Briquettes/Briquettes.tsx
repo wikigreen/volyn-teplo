e74edc7176ca54534b01971b1fb18f50f8e2a17e
wikigreen <vhrynevich20@gmail.com>
2024-09-01 18:23:10 +0300

    Briquettes

--- src/pages/Briquettes/Briquettes.tsx
+++ src/pages/Briquettes/Briquettes.tsx
@@ -2,6 +2,9 @@ import { FC } from "react";
 import Typography from "@mui/material/Typography";
 import { Box, Container } from "@mui/material";
 
+import brick from "../../assets/brick.png";
+import brickRuf from "../../assets/brickRuf.png";
+
 export const Briquettes: FC = () => {
   return (
     <Container maxWidth="md">
@@ -9,6 +12,18 @@ export const Briquettes: FC = () => {
         <Typography variant="h4" component="h1" gutterBottom textAlign="center">
           Брикети
         </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: "auto",
+            width: { sm: "300px", xs: "100%" },
+            float: { sm: "left", xs: "unset" },
+            marginRight: 2,
+            marginBottom: 1,
+          }}
+          alt={"chips"}
+          src={brick}
+        />
         <Typography variant="body1" paragraph>
           Паливні брикети RUF - один з найпоширеніших стандартів паливних
           брикетів. Брикет стандарту RUF (євродрова) виготовляється з тирси на
@@ -17,12 +32,25 @@ export const Briquettes: FC = () => {
           пачки масою 9,7 – 10,3 кг. Пачки укладені на піддон. Кількість на
           піддоні – 96 штук (пачок).
         </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: "auto",
+            width: { sm: "300px", xs: "100%" },
+            float: { sm: "right", xs: "unset" },
+            marginRight: 2,
+            marginBottom: 1,
+          }}
+          alt={"chips"}
+          src={brickRuf}
+        />
         <Typography variant="body1" paragraph>
           Паливні брикети Nestro – це ефективний замінник палива, як природній
           газ, дизпаливо, вугілля, тощо. Брикет маленьких розмірів і мало
           важить. Це дозволяє Вам складати їх, як завгодно, транспортувати без
           особливих зусиль.
         </Typography>
+
         <Typography variant="body1" paragraph>
           Ціна Договірна, можливо з ПДВ та без ПДВ.
         </Typography>

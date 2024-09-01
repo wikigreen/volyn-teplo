c3b1b11b7cb78ec2174ed39f1682859efbca4d8e
wikigreen <vhrynevich20@gmail.com>
2024-09-01 18:12:30 +0300

    Chips images

--- src/pages/WoodChips/WoodChips.tsx
+++ src/pages/WoodChips/WoodChips.tsx
@@ -1,30 +1,51 @@
 import { FC } from "react";
 import Typography from "@mui/material/Typography";
 import { Box, Container } from "@mui/material";
+import woodChips from "../../assets/woodChips.png";
 
 export const WoodChips: FC = () => {
   return (
     <Container maxWidth="md">
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        textAlign="center"
+        fontWeight={700}
+      >
+        Тріска
+      </Typography>
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom textAlign="center">
-          Тріска
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Дерев’яна тріска є ефективним і простим з погляду виготовлення
-          паливним матеріалом. Вона позиціонується як залишки при виробництві
-          пиломатеріалів, які неможливо застосувати в споживчих чи будівельних
-          цілях, як основну сировину. Тим не менш, тріска не втрачає своїх
-          цінностей під час використання.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Невисока вартість у зв’язці з високим ККД горіння створює економічно
-          вигідні умови для опалення будівель. А можливість одночасно поєднувати
-          деревинну тріску з іншими видами палива дозволить коригувати витрати
-          по потребам.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Ціна Договірна, можливо з ПДВ та без ПДВ.
-        </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: "auto",
+            width: "350px",
+            float: "left",
+            marginRight: 2,
+            marginBottom: 1,
+          }}
+          alt={"chips"}
+          src={woodChips}
+        />
+        <Box>
+          <Typography variant="body1" paragraph>
+            Дерев’яна тріска є ефективним і простим з погляду виготовлення
+            паливним матеріалом. Вона позиціонується як залишки при виробництві
+            пиломатеріалів, які неможливо застосувати в споживчих чи будівельних
+            цілях, як основну сировину. Тим не менш, тріска не втрачає своїх
+            цінностей під час використання.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Невисока вартість у зв’язці з високим ККД горіння створює економічно
+            вигідні умови для опалення будівель. А можливість одночасно
+            поєднувати деревинну тріску з іншими видами палива дозволить
+            коригувати витрати по потребам.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Ціна Договірна, можливо з ПДВ та без ПДВ.
+          </Typography>
+        </Box>
       </Box>
     </Container>
   );

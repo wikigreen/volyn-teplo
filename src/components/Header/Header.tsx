9dda5f7acf131475e44e294b97679feb7f3c6bbf
wikigreen <vhrynevich20@gmail.com>
2024-09-01 19:57:53 +0300

    fix build

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import { lightBlue } from "@mui/material/colors";
 import {
   Box,
   Container,
